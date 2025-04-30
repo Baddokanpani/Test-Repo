988ff4c58ea77de2e15796c82f8d7f072f5d219a
Baddokanpani <Rico.tru.suave@gmail.com>
2025-04-30 16:07:25 -0400

    Create data_content.js
    finalizing webpage

---
+++ data_content.js
@@ -0,0 +1,30 @@
+const contentData = [
+    {
+      id: "chicken-parm",
+      name: "Chicken Parmesan",
+      image: "images/recipe-placeholder.png",
+      description: "A crispy breaded chicken breast topped with marinara sauce and melted cheese.",
+      prepTime: 15,
+      cookTime: 30,
+      servings: 4
+    },
+    {
+      id: "chicken-alfredo",
+      name: "Chicken Alfredo",
+      image: "images/recipe-placeholder.png",
+      description: "Creamy Alfredo pasta tossed with grilled chicken slices and herbs.",
+      prepTime: 10,
+      cookTime: 25,
+      servings: 3
+    },
+    {
+      id: "focaccia-bread",
+      name: "Focaccia Bread",
+      image: "images/recipe-placeholder.png",
+      description: "Fluffy, aromatic bread topped with olive oil, rosemary, and sea salt.",
+      prepTime: 20,
+      cookTime: 35,
+      servings: 6
+    }
+  ];
+  
